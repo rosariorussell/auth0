ab009fa53ca650ac5d763a843f0aee75175aca34
Russell Rosario <rosariorussell@gmail.com>
2018-06-24 09:46:26 -0400

    initial commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,67 @@
+const passport = require('passport')
+
+const env = {
+  AUTH0_CLIENT_ID: 'cftd3aHlDCTSoMwbFzFP3krbK6ZLL35h',
+  AUTH0_DOMAIN: 'remotelyjs.auth0.com',
+  AUTH0_CALLBACK_URL: 'http://localhost:5000/callback'
+}
+
+module.exports = (app) => {
+  app.get('/test', (req, res) => {
+    res.send('thanks for logging in')
+  })
+
+  app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }))
+
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/jobListings')
+    })
+
+  app.get('/api/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+  })
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user)
+  })
+
+  // Perform the login
+  app.get(
+    '/login',
+    passport.authenticate('auth0', {
+      clientID: env.AUTH0_CLIENT_ID,
+      domain: env.AUTH0_DOMAIN,
+      redirectUri: env.AUTH0_CALLBACK_URL,
+      audience: 'https://' + env.AUTH0_DOMAIN + '/userinfo',
+      responseType: 'code',
+      scope: 'openid'
+    }),
+    function (req, res) {
+      res.redirect('/')
+    }
+  )
+
+  // Perform session logout and redirect to homepage
+  app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+  })
+
+  // Perform the final stage of authentication and redirect to '/user'
+  app.get(
+    '/callback',
+    passport.authenticate('auth0', {
+      failureRedirect: '/'
+    }),
+    function (req, res) {
+      console.log(req)
+      res.redirect(req.session.returnTo || '/test')
+    }
+  )
+}
---
+++ services/passport.js
@@ -0,0 +1,64 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const Auth0Strategy = require('passport-auth0')
+const keys = require('../config/keys')
+
+const mongoose = require('mongoose')
+const User = mongoose.model('users')
+
+// serialize cookie before sending in header
+// null is error object
+
+// passport.serializeUser((user, done) => {
+//   done(null, user.id)
+// })
+
+// // deserialize cookie to authenticate user
+// // null is error object
+// passport.deserializeUser((id, done) => {
+//   User.findById(id)
+//     .then(user => {
+//       done(null, user)
+//     })
+// })
+
+// passport.authenticate('google')
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback',
+  proxy: true
+},
+async (accessToken, refreshToken, profile, done) => {
+  const existingUser = await User.findOne({ googleId: profile.id })
+
+  if (existingUser) {
+    return done(null, existingUser)
+  }
+
+  const user = await new User({ googleId: profile.id }).save()
+  done(null, user)
+}))
+
+const strategy = new Auth0Strategy(
+  {
+    domain: 'remotelyjs.auth0.com',
+    clientID: 'cftd3aHlDCTSoMwbFzFP3krbK6ZLL35h',
+    clientSecret: keys.auth0,
+    callbackURL: 'http://localhost:5000/callback'
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    return done(null, profile)
+  }
+)
+
+passport.use(strategy)
+
+// This can be used to keep a smaller payload
+passport.serializeUser(function (user, done) {
+  done(null, user)
+})
+
+passport.deserializeUser(function (user, done) {
+  done(null, user)
+})
